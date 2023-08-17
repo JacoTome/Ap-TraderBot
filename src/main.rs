e4065e93ec7b8b75977cd9b8e706ff72767da6fc
JacoTome <jacotomelleri@hotmail.com>
2023-08-17 16:34:08 +0200

    Checkbox, display in order, some refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-// pub mod trader;
 use std::collections::HashMap;
 use std::io::Read;
 use std::net;
 
 use csv;
 use eframe::egui;
-use egui::plot::{BarChart, Line, PlotPoints};
+use egui::plot::{Bar, BarChart, Line, PlotPoints};
 use itertools::Itertools;
 // use trader::Trader;
 // use unitn_market_2022::{good::good_kind::GoodKind, market::good_label::GoodLabel};
@@ -31,9 +30,8 @@ fn main() {
 
 struct MyApp {
     goods: HashMap<String, Vec<f64>>,
-    goods_to_show: HashMap<String, Vec<f64>>,
+    goods_to_show: HashMap<String, (bool, Vec<f64>)>,
     graph_choose: usize,
-    start: bool,
     load_value: usize,
     // market_goods: HashMap<String, Vec<GoodLabel>>,
 }
@@ -41,14 +39,16 @@ struct MyApp {
 impl<'a> MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         let goods = Self::read_from_file();
-        let goods_to_show: HashMap<String, Vec<f64>> = HashMap::new();
+        let mut goods_to_show: HashMap<String, (bool, Vec<f64>)> = HashMap::new();
         let graph_choose = 0;
-        let start = false;
+
+        for (key, _value) in goods.iter() {
+            goods_to_show.insert(key.to_string(), (true, vec![0.0]));
+        }
         Self {
             goods,
             goods_to_show,
             graph_choose,
-            start,
             load_value: 0,
         }
     }
@@ -65,9 +65,9 @@ impl<'a> MyApp {
                 }
                 let val = value.drain(0..tot).collect_vec();
                 if self.goods_to_show.get(key).is_none() {
-                    self.goods_to_show.insert(key.to_string(), val);
+                    self.goods_to_show.get_mut(key).unwrap().1.extend(val);
                 } else {
-                    self.goods_to_show.get_mut(key).unwrap().extend(val);
+                    self.goods_to_show.get_mut(key).unwrap().1.extend(val);
                 }
             }
         }
@@ -81,9 +81,9 @@ impl<'a> MyApp {
             } else {
                 let val = value.remove(0);
                 if self.goods_to_show.get(key).is_none() {
-                    self.goods_to_show.insert(key.to_string(), vec![val]);
+                    self.goods_to_show.get_mut(key).unwrap().1.push(val);
                 } else {
-                    self.goods_to_show.get_mut(key).unwrap().push(val);
+                    self.goods_to_show.get_mut(key).unwrap().1.push(val);
                 }
             }
         }
@@ -91,18 +91,23 @@ impl<'a> MyApp {
 
     fn load_all(&mut self) {
         // Push all goods from self.goods in goods_to_show
-        for (key, value) in self.goods.iter_mut() {
-            if value.len() == 0 {
-                continue;
-            } else {
-                self.goods_to_show.insert(key.to_string(), value.to_vec());
+        self.goods_to_show.clear();
+        if !self.goods.is_empty() {
+            for (key, value) in self.goods.iter_mut() {
+                if value.len() == 0 {
+                    continue;
+                } else {
+                    self.goods_to_show
+                        .insert(key.to_string(), (true, value.to_vec()));
+                }
             }
         }
+        self.goods.clear();
     }
 
     fn read_from_file() -> HashMap<String, Vec<f64>> {
         // Open csv file
-        let mut rdr = csv::Reader::from_path("market.csv").unwrap();
+        let mut rdr = csv::Reader::from_path("../market.csv").unwrap();
 
         let mut ret_val = HashMap::new();
         // get headers
@@ -143,39 +148,49 @@ impl eframe::App for MyApp {
         egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.label("Trader visualization");
-                if ui.button("Read from file").clicked() {
+                let load_all_button = ui.button("Load all");
+                let load_one_button = ui.button("Load one");
+
+                if load_all_button.clicked() {
                     self.load_all();
                 }
-                if ui.button("Read one by one").clicked() {
+                if load_one_button.clicked() {
                     self.load_one();
                 }
 
                 ui.end_row();
                 ui.label("Load how many?");
                 let mut val_string = format!("{}", self.load_value);
+
                 let value = ui.add_sized([50.0, 20.0], egui::TextEdit::singleline(&mut val_string));
+                let load_value = ui.button("Load");
                 if value.changed() {
                     if !val_string.parse::<usize>().is_err() {
                         self.load_value = val_string.parse::<usize>().unwrap();
                     }
                 }
-                if ui.button("Load").clicked() || ui.input().key_pressed(egui::Key::Enter) {
+                if load_value.clicked() || ui.input().key_pressed(egui::Key::Enter) {
                     self.load_value(self.load_value);
                 }
             });
         });
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.label("Trader status");
-            ui.end_row();
+            ui.label("Select goods to show");
+            ui.separator();
+            ui.horizontal_wrapped(|ui| {
+                for (key, _value) in self.goods_to_show.clone().iter() {
+                    ui.checkbox(&mut self.goods_to_show.get_mut(key).unwrap().0, key);
+                }
+            });
         });
         egui::TopBottomPanel::bottom("status_bar").show(ctx, |ui| {
             ui.label("Status bar");
             ui.horizontal_wrapped(|ui| {
                 for (key, value) in self.goods_to_show.iter() {
-                    if value.last().is_none() {
+                    if value.1.last().is_none() {
                         continue;
                     } else {
-                        ui.label(format!("{}: {} || ", key, value.last().unwrap()));
+                        ui.label(format!("{}: {} || \n ", key, value.1.last().unwrap()));
                     }
                 }
             });
@@ -189,13 +204,6 @@ impl eframe::App for MyApp {
                 ui.selectable_value(&mut self.graph_choose, 1, "Bars");
             });
             ui.separator();
-            // if ui.button("Start/Stop").clicked() {
-            //     self.start = !self.start;
-            // }
-
-            // if self.start {
-            //     self.load_one();
-            // }
 
             // Plot
             match self.graph_choose {
@@ -204,6 +212,7 @@ impl eframe::App for MyApp {
                     let _plot = egui::widgets::plot::Plot::new("my_plot").show(ui, |ui| {
                         for (keys, values) in self.goods_to_show.iter() {
                             let points = values
+                                .1
                                 .iter()
                                 .enumerate()
                                 .map(|(i, &v)| [i as f64, v])
@@ -214,45 +223,48 @@ impl eframe::App for MyApp {
                 }
                 1 => {
                     ui.label("Bars");
-                    let bars = self
+                    let values = self
                         .goods_to_show
                         .iter()
-                        .enumerate()
-                        .map(|(i, (key, value))| {
-                            let mut bar =
-                                egui::widgets::plot::Bar::new(i as f64, *value.last().unwrap());
-                            bar = bar.name(key);
-                            bar
-                        })
+                        .map(|(key, value)| (key, (value.0, *value.1.last().unwrap())))
                         .collect_vec();
+                    // Sort values
+                    let values = values
+                        .into_iter()
+                        .sorted_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
+                        .collect_vec();
+                    let mut bars: Vec<Bar> = Vec::new();
+
+                    for (i, bar_val) in values.iter().enumerate() {
+                        if bar_val.1 .0 {
+                            let mut bar = egui::widgets::plot::Bar::new(i as f64, bar_val.1 .1);
+                            bar = bar.name(bar_val.0);
+                            bar = bar
+                                .stroke(egui::Stroke::new(
+                                    1.0,
+                                    egui::Color32::from_rgb(
+                                        ((i * 40) % 255) as u8,
+                                        ((i * 30) % 255) as u8,
+                                        ((i * 20) % 255) as u8,
+                                    ),
+                                ))
+                                .fill(egui::Color32::from_rgb(
+                                    ((i * 40) % 255) as u8,
+                                    ((i * 30) % 255) as u8,
+                                    ((i * 20) % 255) as u8,
+                                ));
+                            bars.push(bar)
+                        }
+                    }
+
                     let _plot = egui::widgets::plot::Plot::new("my_plot").show(ui, |ui| {
                         ui.bar_chart(BarChart::new(bars));
                     });
                 }
-                _ => {}
+                _ => {
+                    ui.label("How did you get here?");
+                }
             }
-
-            // let _plot = egui::widgets::plot::Plot::new("my_plot").show(ui, |ui| {
-            //     for (keys, values) in self.goods_to_show.iter() {
-            //         let points = values
-            //             .iter()
-            //             .enumerate()
-            //             .map(|(i, &v)| [i as f64, v])
-            //             .collect_vec();
-            //         ui.line(Line::new(PlotPoints::new(points)).name(keys));
-            //     }
-            // ui.line(Line::new(PlotPoints::new(
-            //     self.trader.get_good_history(GoodKind::EUR),
-            // )));
-            // ui.line(Line::new(PlotPoints::new(
-            //     self.trader.get_good_history(GoodKind::USD),
-            // )));
-            // ui.line(Line::new(PlotPoints::new(
-            //     self.trader.get_good_history(GoodKind::YEN),
-            // )));
-            // ui.line(Line::new(PlotPoints::new(
-            //     self.trader.get_good_history(GoodKind::YUAN),
-            // )));
         });
     }
 }
